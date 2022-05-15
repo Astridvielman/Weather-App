25659f2b4de69c728af565d4ec78300921b80044
Astridvielman <100819193+Astridvielman@users.noreply.github.com>
2022-05-15 16:59:01 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,78 @@
 
+function formatDate(date) {
+  let hours = date.getHours();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayIndex];
+
+  return `${days[dayIndex]} ${hours}:${minutes}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 73;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 23;
+}
+
+// Feature #1
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+// Feature #2
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+// Feature #3
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+// display the name of the city on the result page and the current temperature of the city.
+
+function cityInput(response) {
+  console.log(response);
+}
+
+let apiKey = "63b53295333e85821148fc1de9533518";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=63b53295333e85821148fc1de9533518";
+
+axios.get(apiUrl).then(cityInput);
